2be82a3b5846854df17843f5777dd6c0fa44c647
Noah Spielman <nspielman@live.com>
2024-01-04 23:19:51 -0700

    refactored with helper funcs

--- handlers/file_handler.go
+++ handlers/file_handler.go
@@ -5,60 +5,43 @@ import (
 	"Smd/types"
 	"crypto/sha256"
 	"encoding/hex"
+	"fmt"
 	"io"
+	"mime/multipart"
 	"net/http"
 	"os"
 	"path/filepath"
 )
 
 type FileHandler struct {
-	fileService services.FileService
+	FileService services.FileService
 }
 
-func UploadFileHandler(w http.ResponseWriter, r *http.Request) {
+func (fh *FileHandler) UploadFileHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
 		return
 	}
 
-	r.Body = http.MaxBytesReader(w, r.Body, 10<<20) // Max memory 10MB
-	err := r.ParseMultipartForm(10 << 20)           // Max memory 10MB
+	file, header, err := parseAndValidateFile(r, w)
 	if err != nil {
-		http.Error(w, "File to big", http.StatusInternalServerError)
-		return
-	}
-
-	file, header, err := r.FormFile("uploadFile")
-	if err != nil {
-		http.Error(w, "Error retrieving the file", http.StatusInternalServerError)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 	defer file.Close()
 
-	hasher := sha256.New()
-	if _, err := io.Copy(hasher, file); err != nil {
+	hashedFilename, err := hashFile(file)
+	if err != nil {
 		http.Error(w, "Error hashing the file", http.StatusInternalServerError)
 		return
 	}
-	hashedFilename := hex.EncodeToString(hasher.Sum(nil))
-	subdirectory := r.FormValue("subdirectory")
-	flatFilename := subdirectory + "_" + hashedFilename
-	filepath := filepath.Join("/StoreMeDaddy", flatFilename)
-	out, err := os.Create(filepath)
+
+	filepath, err := saveFile(file, hashedFilename, r.FormValue("subdirectory"))
 	if err != nil {
-		http.Error(w, "Error creating the file", http.StatusInternalServerError)
+		http.Error(w, "Error saving the file", http.StatusInternalServerError)
 		return
 	}
-	defer out.Close()
-	_, err = file.Seek(0, 0) // Reset file read position
-	if err != nil {
-		http.Error(w, "Error resetting file position", http.StatusInternalServerError)
-		return
-	}
-	if _, err = io.Copy(out, file); err != nil {
-		http.Error(w, "Error writing the file", http.StatusInternalServerError)
-		return
-	}
+
 	f := types.File{
 		Name:        header.Filename,
 		Size:        header.Size,
@@ -66,11 +49,58 @@ func UploadFileHandler(w http.ResponseWriter, r *http.Request) {
 		Location:    filepath,
 		OwnerID:     "1",
 	}
-	fileService := services.NewFileService()
-	if err := fileService.UploadFile(f); err != nil {
+	if err := fh.FileService.UploadFile(f); err != nil {
 		http.Error(w, "Error MetaData not saved", http.StatusInternalServerError)
 	}
+
 	w.Header().Set("Content-Type", "text/plain")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("File uploaded successfully"))
+}
+
+func parseAndValidateFile(r *http.Request, w http.ResponseWriter) (file multipart.File, header *multipart.FileHeader, err error) {
+	r.Body = http.MaxBytesReader(w, r.Body, 10<<20) // Max memory 10MB
+	err = r.ParseMultipartForm(10 << 20)            // Max memory 10MB
+	if err != nil {
+		return nil, nil, fmt.Errorf("file too big")
+	}
+
+	file, header, err = r.FormFile("uploadFile")
+	if err != nil {
+		return nil, nil, fmt.Errorf("error retrieving the file")
+	}
+
+	return file, header, nil
+}
+
+func hashFile(file multipart.File) (hashedFilename string, err error) {
+	hasher := sha256.New()
+	if _, err := io.Copy(hasher, file); err != nil {
+		return "", err
+	}
+	hashedFilename = hex.EncodeToString(hasher.Sum(nil))
+
+	return hashedFilename, nil
+}
+
+func saveFile(file multipart.File, hashedFilename, subdirectory string) (path string, err error) {
+	flatFilename := subdirectory + "_" + hashedFilename
+
+	path = filepath.Join("/StoreMeDaddy", flatFilename)
+	out, err := os.Create(path)
+	if err != nil {
+		return "", err
+	}
+	defer out.Close()
+
+	_, err = file.Seek(0, 0) // Reset file read position
+	if err != nil {
+		return "", err
+	}
+
+	if _, err = io.Copy(out, file); err != nil {
+		return "", err
+	}
+
+	return path, nil
 }
